d9ac1234cf9147d26f939218b61a2e242d3393ed
Maxwell Sultan <maxsultankent@gmail.com>
2021-08-24 11:20:27 -0600

    formatted code with prettier

--- js/events.js
+++ js/events.js
@@ -4,11 +4,11 @@ const listen = (sel, event) => {
       return true;
     }
     if (sel === 'button[name="js-settings-button"]' && event === 'click') {
-      toggleElement(settingsForm)
-      if (settingsButton.innerText === "Show Settings") {
-        settingsButton.innerText = "Hide Settings"
-      } else if (settingsButton.innerText === "Hide Settings") {
-        settingsButton.innerText = "Show Settings"
+      toggleElement(settingsForm);
+      if (settingsButton.innerText === 'Show Settings') {
+        settingsButton.innerText = 'Hide Settings';
+      } else if (settingsButton.innerText === 'Hide Settings') {
+        settingsButton.innerText = 'Show Settings';
       }
     }
     if (sel === 'select[name="image-quality"]' && event === 'change') {
@@ -32,17 +32,39 @@ const listen = (sel, event) => {
     if (sel === 'input[name="js-box-file"]' && event === 'change') {
       file = ev.target.files[0]; // get the file
       Mini.compressResizeBlobify(file, {
-        aspectRatioPreserved: aspectRatioPreserved,
-        inputWidth: inputWidth,
-        inputHeight: inputHeight,
-        smoothingOptions: smoothingOptions,
-        quality: quality
-      }).then(renderImageData)
+        aspectRatioPreserved,
+        inputWidth,
+        inputHeight,
+        smoothingOptions,
+        quality,
+      })
+        .then(renderImageData)
+        .catch((error) => {
+          alert(error);
+          clearInputs();
+        });
 
-      $('input[name="js-box-file"]').value = "";
+      $('input[name="js-box-file"]').value = '';
     }
-    if (['form[name="js-box"]', 'div[name="js-box-input"]', 'input[name="js-box-file"]', 'label[name="js-box-file-label"]'].includes(sel)) {
-      if (['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop'].includes(event)) {
+    if (
+      [
+        'form[name="js-box"]',
+        'div[name="js-box-input"]',
+        'input[name="js-box-file"]',
+        'label[name="js-box-file-label"]',
+      ].includes(sel)
+    ) {
+      if (
+        [
+          'drag',
+          'dragstart',
+          'dragend',
+          'dragover',
+          'dragenter',
+          'dragleave',
+          'drop',
+        ].includes(event)
+      ) {
         ev.preventDefault();
         // ev.stopPropagation();
       }
@@ -54,39 +76,64 @@ const listen = (sel, event) => {
       }
       if (event === 'drop') {
         file = ev.dataTransfer.files[0];
-        Mini.compressResizeBlobify(file, {aspectRatioPreserved, inputWidth, inputHeight, smoothingOptions, quality}).then(renderImageData)
+        Mini.compressResizeBlobify(file, {
+          aspectRatioPreserved,
+          inputWidth,
+          inputHeight,
+          smoothingOptions,
+          quality,
+        })
+          .then(renderImageData)
+          .catch((error) => {
+            alert(error);
+            clearInputs();
+          });
       }
     }
   });
-}
+};
+
 listen('button[name="js-settings-button"]', 'click');
 listen('select[name="image-quality"]', 'change');
 listen('select[name="smoothing-options"]', 'change');
 listen('input[name="js-aspect-ratio"]', 'change');
 listen('input[name="js-height"]', 'change');
 listen('input[name="js-width"]', 'change');
-listen('label[name="js-box-file-label"]',)
+listen('label[name="js-box-file-label"]');
 
 listen('input[name="js-box-file"]', 'change');
 [
   'form[name="js-box"]',
   'div[name="js-box-input"]',
   'input[name="js-box-file"]',
-  'label[name="js-box-file-label"]'
-].forEach(formItem => {
-  ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop'].forEach(event => {
-    listen(formItem, event);
-  })
-})
+  'label[name="js-box-file-label"]',
+].forEach((formItem) => {
+  ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop'].forEach(
+    (event) => {
+      listen(formItem, event);
+    },
+  );
+});
 
-window.addEventListener("paste", (e) => {
-  var item = Array.from(e.clipboardData.items).find(x => /^image\//.test(x.type));
+window.addEventListener('paste', (e) => {
+  var item = Array.from(e.clipboardData.items).find((x) => /^image\//.test(x.type));
   try {
     file = item.getAsFile();
   } catch {
-    alert('This is not an image element')
+    alert('This is not an image element');
     return;
   }
 
-  Mini.compressResizeBlobify(file, {aspectRatioPreserved, inputWidth, inputHeight, smoothingOptions, quality}).then(renderImageData)
+  Mini.compressResizeBlobify(file, {
+    aspectRatioPreserved,
+    inputWidth,
+    inputHeight,
+    smoothingOptions,
+    quality,
+  })
+    .then(renderImageData)
+    .catch((error) => {
+      alert(error);
+      clearInputs();
+    });
 });
--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 const $ = (sel, el) => {
   return (el || document.body).querySelector(sel);
-}
+};
 
-const MIME_TYPE = "image/jpeg";
+const MIME_TYPE = 'image/jpeg';
 let quality = parseFloat($('select[name="image-quality"]').value, 10);
-let smoothingOptions = "bi-linear";
+let smoothingOptions = 'bi-linear';
 let inputHeight;
 let inputWidth;
 let aspectRatioPreserved = true;
@@ -15,58 +15,58 @@ const settingsForm = $('form[class="js-settings-form"]');
 const dragAndDropForm = $('form[name="js-box"]');
 
 const displayInfo = (label, file, parentElement) => {
-  const p = document.createElement("p");
+  const p = document.createElement('p');
   p.innerText = `${label} - ${readableBytes(file.size)}`;
   parentElement.append(p);
-}
+};
 
 const displayDownloadLink = (linkText, objUrl, dataUrl, parentElement) => {
-
-  let copybutton = document.createElement("button");
-  copybutton.innerText = "Copy Data URL";
+  let copybutton = document.createElement('button');
+  copybutton.innerText = 'Copy Data URL';
   copybutton.onclick = () => {
-    navigator.clipboard.writeText(dataUrl)
+    navigator.clipboard.writeText(dataUrl);
     // TODO: UI should reflect a successful or failed copy
-  }
+  };
   parentElement.append(copybutton);
 
-  var downloadLink = document.createElement("a");
+  var downloadLink = document.createElement('a');
   downloadLink.innerText = linkText;
   downloadLink.href = objUrl;
-  downloadLink.download = "download";
+  downloadLink.download = 'download';
   parentElement.append(downloadLink);
-}
+};
 
 const readableBytes = (bytes) => {
   const i = Math.floor(Math.log(bytes) / Math.log(1024)),
-    sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 
-  return (bytes / Math.pow(1024, i)).toFixed(2) + " " + sizes[i];
-}
+  return (bytes / Math.pow(1024, i)).toFixed(2) + ' ' + sizes[i];
+};
 
 const toggleElement = (element) => {
-  if (element.style.display === "none") {
-    element.style = ""
-  } else if (element.style.display === "") {
-    element.style = "display: none"
+  if (element.style.display === 'none') {
+    element.style = '';
+  } else if (element.style.display === '') {
+    element.style = 'display: none';
   }
-}
+};
 
 const createImageDiv = () => {
-  let currentDiv = document.createElement("div");
-  $(".js-images-container").append(currentDiv);
+  let currentDiv = document.createElement('div');
+  $('.js-images-container').append(currentDiv);
   return currentDiv;
-}
+};
 
-const renderImageData = ({blob, canvas, dataUrl, objUrl}) => {
-    let div = createImageDiv();
-    div.append(canvas);
-    displayInfo("Original file", file, div);
-    displayInfo("Compressed file", blob, div);
-    displayDownloadLink("Download Minified Image", objUrl, dataUrl, div);
-    clearInputs()
-}
+const renderImageData = ({ blob, canvas, dataUrl, objUrl }) => {
+  let div = createImageDiv();
+  div.append(canvas);
+  displayInfo('Original file', file, div);
+  displayInfo('Compressed file', blob, div);
+  displayDownloadLink('Download Minified Image', objUrl, dataUrl, div);
+  clearInputs();
+};
 
 const clearInputs = () => {
-  $('input[name="js-box-file"]').value = "";
-}
+  $('input[name="js-box-file"]').value = '';
+  $('input[name="js-img-paste-box"]').value = '';
+};
